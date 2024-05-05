8d5b0bb389fe3d9ea664d9b8cb483a411ba62efe
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-05-06 02:03:42 +0300

    layout, theming, entityframework bloat, createView, theming

---
+++ KitapPazariWeb/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using KitapPazariWeb.Data;
+using KitapPazariWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KitapPazariWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _context.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+    }
+}
