bdc04f582fee49b14db7575b0dc63e4fff0948d8
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-05-05 19:05:56 +0300

    Add project files.

---
+++ KitapPazariWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KitapPazariWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
