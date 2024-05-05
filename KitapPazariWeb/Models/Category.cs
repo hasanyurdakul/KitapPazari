e1c4115f96c95a16ef3e439d76795482d9cedbbf
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-05-05 22:43:36 +0300

    packages added

---
+++ KitapPazariWeb/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace KitapPazariWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
