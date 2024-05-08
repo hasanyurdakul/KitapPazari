445b03b2e454dd85c6f3e2e5cbc5b773c8f0dd93
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-05-08 14:45:07 +0300

    seperated SeedDatas into a different folder

--- KitapPazariDataAccess/Data/ApplicationDbContext.cs
+++ KitapPazariDataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using KitapPazariModels;
+﻿using KitapPazariDataAccess.Seed;
+using KitapPazariModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Client;
 
@@ -8,17 +9,16 @@ namespace KitapPazariDataAccess.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-
+            
         }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Category>().HasData(
-                new Category() { Id = 1, Name = "Action", DisplayOrder = 1 },
-                new Category() { Id = 2, Name = "SciFi", DisplayOrder = 2 },
-                new Category() { Id = 3, Name = "History", DisplayOrder = 3 }
-                );
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new CategorySeed());
         }
+
     }
 }
 
---
+++ KitapPazariDataAccess/Seed/CategorySeed.cs
@@ -0,0 +1,23 @@
+﻿using KitapPazariModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitapPazariDataAccess.Seed
+{
+    public class CategorySeed : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+                new Category() { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category() { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category() { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
