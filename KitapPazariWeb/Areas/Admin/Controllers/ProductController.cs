1ab543abc838dc2603715055358238055b991a7c
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-05-10 04:22:48 +0300

    implemented DataTables.js

--- KitapPazariWeb/Areas/Admin/Controllers/ProductController.cs
+++ KitapPazariWeb/Areas/Admin/Controllers/ProductController.cs
@@ -126,5 +126,16 @@ namespace KitapPazariWeb.Areas.Admin.Controllers
                 return RedirectToAction("Index");
             }
         }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new { data = objProductList }); 
+        }
+
+        #endregion
     }
 }
---
+++ KitapPazariWeb/wwwroot/js/product.js
@@ -0,0 +1,19 @@
+﻿
+
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable(){
+    dataTable = $('#dataTable').DataTable({
+        "ajax": { url: '/admin/product/getall' },
+        "columns": [
+            { data: 'title', "width": "15%" },
+            { data: 'isbn', "width": "15%" },
+            { data: 'listPrice', "width": "15%" },
+            { data: 'author', "width": "15%" },
+            { data: 'category.name', "width": "15%" }
+        ]
+    });
+}
