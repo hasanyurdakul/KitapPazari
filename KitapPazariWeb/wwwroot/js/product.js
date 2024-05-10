983dc05e0379741ae811b4dcd31f0929abb1c9e8
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-05-10 04:38:53 +0300

    added buttons to dataTable in Product->Index

--- KitapPazariWeb/wwwroot/js/product.js
+++ KitapPazariWeb/wwwroot/js/product.js
@@ -13,7 +13,14 @@ function loadDataTable(){
             { data: 'isbn', "width": "15%" },
             { data: 'listPrice', "width": "15%" },
             { data: 'author', "width": "15%" },
-            { data: 'category.name', "width": "15%" }
+            { data: 'category.name', "width": "15%" },
+            {
+                data: 'id', "render": function (data) {
+                    return ` <div class="w-75 btn-group" role="group">
+                         <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2" >  <i class="bi bi-pencil-square"></i> Edit </a>
+                         <a   class="btn btn-danger mx-2" >  <i class="bi bi-trash-fill"></i> Delete </a>
+                    </div> `
+            }   , "width": "15%" }
         ]
     });
 }
