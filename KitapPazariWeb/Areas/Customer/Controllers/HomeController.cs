976cf71f800971003a4430fce43a5e50a3efe348
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-05-13 20:05:32 +0300

    added IndexView, added ProductDetails View

--- KitapPazariWeb/Areas/Customer/Controllers/HomeController.cs
+++ KitapPazariWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using KitapPazariDataAccess.Repository.IRepository;
 using KitapPazariModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,15 +9,25 @@ namespace KitapPazariWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == id,includeProperties:"Category");
+
+            return View(product);
         }
 
         public IActionResult Privacy()
