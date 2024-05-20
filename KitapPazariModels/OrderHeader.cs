7af8215ac55f3ced532c38b2315d5f4c75c27755
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-05-20 19:10:10 +0300

    updated OrderHeader model

--- KitapPazariModels/OrderHeader.cs
+++ KitapPazariModels/OrderHeader.cs
@@ -29,6 +29,7 @@ namespace KitapPazariModels
         public DateTime PaymentDate { get; set; }
         public DateOnly PaymentDueDate { get; set; }
 
+        public string? SessionId { get; set; }
         public string? PaymentIntendId { get; set; }
 
         [Required]
