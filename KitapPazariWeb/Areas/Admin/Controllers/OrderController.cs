694042189c727ac20dd14176043f1cccfbbbe031
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-05-20 23:17:29 +0300

    updated OrderController and it's Index View

--- KitapPazariWeb/Areas/Admin/Controllers/OrderController.cs
+++ KitapPazariWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,7 +1,9 @@
 ﻿using KitapPazariDataAccess.Repository.IRepository;
 using KitapPazariModels;
+using KitapPazariUtility;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace KitapPazariWeb.Areas.Admin.Controllers
 {
@@ -21,9 +23,36 @@ namespace KitapPazariWeb.Areas.Admin.Controllers
 
         #region API CALLS
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
-            List<OrderHeader> objOrderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties:"ApplicationUser").ToList();
+            IEnumerable<OrderHeader> objOrderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties:"ApplicationUser").ToList();
+
+
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaderList = objOrderHeaderList.Where(o => o.PaymentStatus == StaticDetails.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeaderList = objOrderHeaderList.Where(o => o.OrderStatus == StaticDetails.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaderList = objOrderHeaderList.Where(o => o.OrderStatus == StaticDetails.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaderList = objOrderHeaderList.Where(o => o.OrderStatus == StaticDetails.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
+
+
+
+
+
+
+
             return Json(new { data = objOrderHeaderList });
         }
         #endregion
--- KitapPazariWeb/wwwroot/js/order.js
+++ KitapPazariWeb/wwwroot/js/order.js
@@ -1,14 +1,29 @@
 ﻿var dataTable;
 
-   
+
 
 $(document).ready(function () {
-    loadDataTable();
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess")
+    } else {
+        if (url.includes("completed")) {
+            loadDataTable("completed")
+        } else {
+            if (url.includes("pending")) {
+                loadDataTable("pending")
+            } else {
+                if (url.includes("approved")) {
+                    loadDataTable("approved")
+                } else { loadDataTable("all") }
+            }
+        }
+    }
 });
 
-function loadDataTable() {
+function loadDataTable(status) {
     dataTable = $('#dataTable').DataTable({
-        "ajax": { url: '/admin/order/getall' },
+        "ajax": { url: '/admin/order/getall?status='+status },
         "columns": [
             { data: 'id', "width": "5%" },
             { data: 'name', "width": "15%" },
